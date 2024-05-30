fa5107a4a532f0a5a642e90d0ccdcc88d8d4156c
Revazashvili <levanrevazashvili@gmail.com>
2024-05-31 01:03:51 +0400

    add selection sort algorithm

---
+++ src/Extensions/ArrayExtensions.cs
@@ -0,0 +1,63 @@
+﻿namespace Extensions;
+
+public static class ArrayExtensions
+{
+    /// <summary>
+    /// Finds smallest element in array
+    /// </summary>
+    public static (int Element, int Index) FindSmallest(int[] arr)
+    {
+        var smallestIndex = 0;
+        var smallest = arr[smallestIndex];
+        
+        for (var i = 0; i < arr.Length; i++)
+        {
+            var current = arr[i];
+            if (current > smallest) 
+                continue;
+            
+            smallestIndex = i;
+            smallest = current;
+        }
+
+        return (smallest, smallestIndex);
+    }
+    
+    /// <summary>
+    /// Swaps elements in array
+    /// </summary>
+    public static void Swap(int[] arr, int firstElementIndex, int secondElementIndex)
+    {
+        /*
+         same as this code
+         
+           var temp = arr[firstElementIndex];
+           arr[firstElementIndex] = arr[secondElementIndex];
+           arr[secondElementIndex] = temp;
+         */
+        
+        (arr[firstElementIndex], arr[secondElementIndex]) = (arr[secondElementIndex], arr[firstElementIndex]);
+    }
+    
+    /// <summary>
+    /// Prints elements in array
+    /// </summary>
+    public static void Print(IEnumerable<int> arr)
+    {
+        Console.WriteLine(string.Join(", ", arr));
+    }
+    
+    /// <summary>
+    /// Prints elements in array
+    /// </summary>
+    public static void RemoveAt(ref int[] arr, int index)
+    {
+        var length = arr.Length;
+        for (var i = index + 1; i < length; i++)
+        {
+            arr[i - 1] = arr[i];
+        }
+        
+        Array.Resize(ref arr, length - 1);
+    }
+}
--- src/SelectionSort/Program.cs
+++ src/SelectionSort/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Extensions;
 
-Console.WriteLine("Hello, World!");
+int[] arr = [3, 2, 10, 4, 11];
+Console.WriteLine("Before:");
+ArrayExtensions.Print(arr);
+
+var result = SelectionSort.Run(arr);
+
+Console.WriteLine("After:");
+ArrayExtensions.Print(result);
+
+/// <summary>
+/// Selection sort algorithm finds smallest element to passed array, removes it and appends to another sorted array
+/// </summary>
+internal static class SelectionSort
+{
+    internal static int[] Run(int[] arr)
+    {
+        var length = arr.Length;
+        var result = new int[length];
+        
+        for (var i = 0; i < length; i++)
+        {
+            var smallest = ArrayExtensions.FindSmallest(arr);
+            
+            result[i] = smallest.Element;
+            
+            ArrayExtensions.RemoveAt(ref arr, smallest.Index);
+        }
+
+        return result;
+    }
+}
