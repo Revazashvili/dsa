cb8aa355a299910f6239872625de3cf871bac272
Revazashvili <levanrevazashvili@gmail.com>
2024-06-02 20:44:01 +0400

    calculate factorial using linq Aggregate method

--- src/Recursion/Program.cs
+++ src/Recursion/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 Console.WriteLine("Factorial of 5 (5 * 4 * 3 * 2 * 1 = 120)");
 Console.WriteLine(Factorial.Run(5));
+Console.WriteLine(Factorial.UsingLinqAggregate(5));
 
 /// <summary>
 /// Calculates passed number's factorial using recursion
@@ -11,7 +12,12 @@ internal static class Factorial
     {
         if (number == 1)
             return 1;
-        else
-            return number * Run(number - 1);
+        
+        return number * Run(number - 1);
+    }
+    
+    internal static int UsingLinqAggregate(int number)
+    {
+        return number == 1 ? 1 : Enumerable.Range(1, number).Aggregate((first, second) => first * second);
     }
 }
