f7d526cb1570d4d4a72b2268f62f3ca1702c03ff
Revazashvili <levanrevazashvili@gmail.com>
2024-06-02 12:24:51 +0400

    write factorial method using recursion

---
+++ src/Recursion/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Factorial of 5 (5 * 4 * 3 * 2 * 1 = 120)");
+Console.WriteLine(Factorial.Run(5));
+
+/// <summary>
+/// Calculates passed number's factorial using recursion
+/// </summary>
+internal static class Factorial
+{
+    internal static int Run(int number)
+    {
+        if (number == 1)
+            return 1;
+        else
+            return number * Run(number - 1);
+    }
+}
