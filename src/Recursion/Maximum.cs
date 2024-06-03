221c295afdfdbb4fb617a72267b20c2644d14186
Revazashvili <levanrevazashvili@gmail.com>
2024-06-03 21:35:08 +0400

    use expression syntax and Math.Max method

--- src/Recursion/Maximum.cs
+++ src/Recursion/Maximum.cs
@@ -5,12 +5,6 @@ internal static class Maximum
 {
     internal static int Run(int[] arr)
     {
-        if (arr.Length == 0)
-            return 0;
-
-        var max = arr[0];
-        var target = Run(arr[1..]);
-
-        return max > target ? max : target;
+        return arr.Length == 0 ? 0 : Math.Max(arr[0], Run(arr[1..]));
     }
 }
