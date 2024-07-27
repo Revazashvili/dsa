49ea1e8cb1f048822aaa52f87d8f956b91f317f0
Revazashvili <levanrevazashvili@gmail.com>
2024-07-27 13:34:43 +0400

    add program to print all files in specified dir and sub dirs using bfs

---
+++ src/FileDirectories/Program.cs
@@ -0,0 +1,27 @@
+﻿FileDirectories.Run(Directory.GetCurrentDirectory());
+
+internal static class FileDirectories
+{
+    internal static void Run(string startDirectory)
+    {
+        var queue = new Queue<string>();
+        queue.Enqueue(startDirectory);
+
+        while (queue.Count != 0)
+        {
+            var directory = queue.Dequeue();
+            var fileSystemEntries = Directory.GetFileSystemEntries(directory);
+            
+            foreach (var subDirectory in fileSystemEntries)
+            {
+                if (IsFile(subDirectory))
+                    Console.WriteLine(subDirectory);
+                else
+                    queue.Enqueue(subDirectory);
+            }
+            
+        }
+    }
+
+    private static bool IsFile(string path) => File.Exists(path);
+}
